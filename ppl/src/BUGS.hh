d4fd129a9badae810663cef7326f16cb03827ca5
Pablo <pablofcastro@gmail.com>
2026-04-27 14:54:25 -0300

    ppl folder removed

--- ppl/src/BUGS.hh
+++
@@ -1,4 +0,0 @@
-#ifndef PPL_BUGS_hh
-#define PPL_BUGS_hh 1
-extern const char* const BUGS_array[23];
-#endif // !defined(PPL_BUGS_hh)
--- ppl/src/CREDITS.hh
+++
@@ -1,4 +0,0 @@
-#ifndef PPL_CREDITS_hh
-#define PPL_CREDITS_hh 1
-extern const char* const CREDITS_array[446];
-#endif // !defined(PPL_CREDITS_hh)
